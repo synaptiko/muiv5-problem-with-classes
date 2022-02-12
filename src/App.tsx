3f83d4380103bf54a0cc5bf7606b24c058ad2715
Jiří Prokop <jiri@bayzat.com>
2022-02-12 11:19:05 +0100

    Create example of the problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,82 @@
+import Container from '@mui/material/Container';
+import Chip, { chipClasses } from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import { makeStyles, withStyles } from "tss-react/mui";
+import { Typography } from '@mui/material';
+
+const useStylesActual = makeStyles()((theme) => ({
+	root: {
+		backgroundColor: theme.palette.primary.main,
+		color: theme.palette.primary.contrastText
+	},
+	deleteIcon: {
+		color: theme.palette.primary.contrastText,
+		'&:hover': {
+			color: `${theme.palette.primary.contrastText}6`
+		}
+	}
+}));
+
+const StyledChipActual = withStyles(Chip, (theme) => ({
+	root: {
+		backgroundColor: theme.palette.primary.main,
+		color: theme.palette.primary.contrastText
+	},
+	deleteIcon: {
+		color: theme.palette.primary.contrastText,
+		'&:hover': {
+			color: `${theme.palette.primary.contrastText}6`
+		}
+	}
+}));
+
+const useStylesExpected = makeStyles()((theme) => ({
+	root: {
+		backgroundColor: theme.palette.primary.main,
+		color: theme.palette.primary.contrastText,
+		[`& .${chipClasses.deleteIcon}`]: {
+			color: theme.palette.primary.contrastText,
+			'&:hover': {
+				color: `${theme.palette.primary.contrastText}6`
+			}
+		}
+	},
+}));
+
+const StyledChipExpected = withStyles(Chip, (theme, _props, classes) => ({
+	root: {
+		backgroundColor: theme.palette.primary.main,
+		color: theme.palette.primary.contrastText,
+		[`& .${classes.deleteIcon}`]: {
+			color: theme.palette.primary.contrastText,
+			'&:hover': {
+				color: `${theme.palette.primary.contrastText}6`
+			}
+		}
+	},
+}));
+
 function App() {
+	const { classes: classesActual } = useStylesActual();
+	const { classes: classesExpected } = useStylesExpected();
+
   return (
-    <div></div>
+		<Container maxWidth="md" sx={{ my: 4 }}>
+			<Stack direction="column" spacing={1}>
+				<Typography variant="h4">Actual</Typography>
+				<Stack direction="row" spacing={1}>
+					<Chip label="Default chip" onDelete={() => {}} />
+					<Chip label="Chip with classes" onDelete={() => {}} classes={classesActual} />
+					<StyledChipActual label="Styled chip" onDelete={() => {}} />
+				</Stack>
+				<Typography variant="h4">Expected</Typography>
+				<Stack direction="row" spacing={1}>
+					<Chip label="Default chip" onDelete={() => {}} />
+					<Chip label="Chip with classes" onDelete={() => {}} classes={classesExpected} />
+					<StyledChipExpected label="Styled chip" onDelete={() => {}} />
+				</Stack>
+			</Stack>
+		</Container>
   );
 }
 
